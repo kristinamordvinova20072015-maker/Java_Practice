ae1cccc20eada5dfd19f1a4ea405cafad3a1a4af
Kristina <kristinamordvinova20072015@gmail.com>
2025-12-17 11:33:24 +0300

    Task1.java

--- practice21/Task1.java
+++ practice21/Task1.java
@@ -2,24 +2,35 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Класс Task1 демонстрирует использование обобщённого метода
+ * для преобразования массива любого типа данных в список.
+ */
 public class Task1 {
 
-    // Универсальный обобщённый метод
+    /**
+     * Универсальный обобщённый метод convertArrayToList.
+     * Принимает массив элементов типа T и возвращает список List<T>.
+     *
+     * @param array массив элементов произвольного ссылочного типа
+     * @param <T>   параметр обобщённого типа
+     * @return список, содержащий элементы переданного массива
+     */
     public static <T> List<T> convertArrayToList(T[] array) {
+        // Arrays.asList создаёт список на основе массива,
+        // а ArrayList делает его изменяемым
         return new ArrayList<>(Arrays.asList(array));
     }
 
+    /**
+     * Точка входа в программу.
+     * В методе main выполняется проверка работы метода
+     * convertArrayToList на массивах строк и чисел.
+     */
     public static void main(String[] args) {
 
-        // Пример: массив строк
+        // Пример использования метода с массивом строк
         String[] arrStrings = {"one", "two", "three"};
         List<String> listStrings = convertArrayToList(arrStrings);
-        System.out.println("Строки: " + listStrings);
-
-        // Пример: массив чисел (Integer)
-        Integer[] arrNumbers = {1, 2, 3, 4};
-        List<Integer> listNumbers = convertArrayToList(arrNumbers);
-        System.out.println("Числа: " + listNumbers);
-    }
-}
+        System.out.printl
 
